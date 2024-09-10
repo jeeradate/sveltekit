ecdc2e881082fad823d82844aff7052774715a8e
jeeradate <jeeradate@gmail.com>
2024-09-10 16:38:55 +0700

    home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,49 +1,27 @@
-<body>
-	<div class="container">
-		<div class="box" id="box1">1</div>
-		<div class="box" id="box2">2</div>
-		<div class="box" id="box3">3</div>
-		<div class="box" id="box4">4</div>
-		<div class="box" id="box1">5</div>
-		<div class="box" id="box2">6</div>
-		<div class="box" id="box3">7</div>
-		<div class="box" id="box4">8</div>
-	</div>
-	<div style="margin: 20px;">
-		<h2>Let resize your browser window and see how css will handle the different screen size</h2>
+<body style="margin: 20px;">
+	<h1>สวัสดีครับ</h1>
+
+	<p style="font-size: 1.5em ;">
+		เมื่อคุณเขียน App หรือ เว็บด้วย Svelte หรือ Sveltekit แล้วก็ย่อมอยากดู App หรือเว็บใน Hosting
+		จริงๆ เหมือนเว็บนี้ ว่าจะเป็นอย่างไร ซึ่งก็มีหลายวิธี แต่วิธีที่ผมคิดว่าดีและสะดวก คือการใช้ GitHub ช่วย เพราะ
+		GitHub จะช่วยรักษา Version ต่างๆ ของ App หรือเว็บของคุณได้ด้วย <br />
+		วิธีการ
+	</p>
+
+	<div style="font-size: 1.5em;">
+		<ul>
+			<li>ลงทะเบียน <a href="https://github.com/">GitHub</a></li>
+			<li>ลงทะเบียน <a href="https://vercel.com/">Vercel</a></li>
+			<li>เปิด Project ใน VSCode upload ขึ้น GitHub</li>
+			<li>
+				จากนั้นเข้าเว็ป Vercel โดยการ Login ด้วย GitHub และเลือก Project ใน GitHub เพื่อ Deploy
+				เท่านั้นก็จบครับ
+			</li>
+			<li>
+				ดูคลิปนี้เพื่อดูรายละเอียดนะครับ <a href="https://youtu.be/nyPCEH2xuhM?si=qs6AoRuLXy_3SeBL" target="_blank"
+					>สอน Deploy Next.js เว็บไซต์ขึ้น Web Hosting ฟรี ด้วย GitHub + Vercel - YouTube</a
+				>[]()
+			</li>
+		</ul>
 	</div>
 </body>
-
-<style>
-	.container {
-		display: flex;
-		border-style: dashed;
-		justify-content: space-evenly;
-		height: 50vh;
-		align-items: center;
-		flex-wrap: wrap;
-	}
-	.box {
-		width: 150px;
-		height: 150px;
-		font-size: 8em;
-		color: white;
-		text-align: center;
-		border-radius: 15px;
-	}
-	#box1 {
-		background-color: rgb(219, 24, 24);
-	}
-
-	#box2 {
-		background-color: rgb(210, 223, 34);
-	}
-
-	#box3 {
-		background-color: rgb(15, 217, 83);
-	}
-
-	#box4 {
-		background-color: rgb(24, 65, 216);
-	}
-</style>
